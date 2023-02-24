0bc17deb49e04a9077558d66454545cf6abb80b4
Elvea Huang <elvea.huang@gmail.com>
2023-02-24 12:30:55 +0800

    一直在整理，一直在重构，不是在整理就是在重构。

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
+import { createBrowserHistory, createHashHistory } from 'history';
 import Home from '@/pages/Home';
 import '@/theme/ionic.scss';
 
 setupIonicReact();
+const history = createHashHistory();
 
 const App: React.FC = () => (
     <IonApp>
-        <IonReactRouter>
+        <IonReactRouter history={history}>
             <IonRouterOutlet>
                 <Route exact path="/home">
                     <Home />
